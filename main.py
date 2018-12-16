844808fa4e437db179930bdba1656856f82c03a7
Grant Duffy <grantd@buchanan-a.com>
2018-12-15 20:22:59 -0800

    Added request parsing and POST for /state.json

--- main.py
+++ main.py
@@ -3,14 +3,48 @@ esp.osdebug(None)
 import time
 import socket
 import machine
+import uio
+import ure
+import json
 
 led1 = machine.Pin(2, machine.Pin.OUT)
 led2 = machine.Pin(16, machine.Pin.OUT)
 
-def connect():
-    import network
+
+def send_response(status, body=''):
+    cl.send(bytes(status + '\r\n\r\n' + body, 'utf-8'))
+
+
+def parse_request(request):
+    output = {}
+    with uio.BytesIO(request) as f:
+        output['Type'], output['Path'], output['Version'] = [x.decode('utf-8').strip() for x in f.readline().split()]
+        while True:
+            x = f.readline()
+            if x == b'':
+                break
+            result = ure.match(b'(.+): (.+)', x)
+            if result:
+                key = result.group(0).strip()
+                val = result.group(1).strip()
+                output[key] = val
+            if x == b'\r\n':
+                output['Body'] = f.read().decode('utf-8')
+    return output
+
+
+def load_resources():
+    global essid, password, html, javascript
     with open('network.txt', 'r') as f:
         essid, password = [x.strip() for x in f.readlines()]
+    with open('home.html', 'r') as f:
+        html = f.read()
+    with open('scripts.js', 'r') as f:
+        javascript = f.read()
+
+
+def connect():
+    import network
     sta_if = network.WLAN(network.STA_IF)
     sta_if.active(True)
     sta_if.connect(essid, password)
@@ -32,20 +66,40 @@ def connect():
 
 
 def start_server():
+    global cl
     run = True
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('', 80))
     s.listen(5)
     while run:
         cl, addr = s.accept()
-        led1.off()
-        request = cl.recv(1024).decode('utf-8')
-        cl.send(bytes("<body>Hello World</body>", 'utf-8'))
-        print('Connection Received\nClient: {}, Address: {}\n####Request####\n{}'.format(cl, addr, request))
-        led1.on()
+        request = cl.recv(1024)
+        # print('#### GOT REQUEST ####\n{}\n'.format(request.decode('utf-8')))
+        request_data = parse_request(request)
+        if request_data['Type'] == 'GET':
+            if request_data['Path'] == '/':
+                send_response('HTTP/1.1 200 OK', html)
+            if request_data['Path'] == '/scripts.js':
+                send_response('HTTP/1.1 200 OK', javascript)
+        if request_data['Type'] == 'POST':
+            if request_data['Path'] == '/state.json':
+                command = json.loads(request_data['Body'])
+                if 'led1' in command:
+                    led1.off() if command['led1'] == 1 else led1.on()
+                if 'led2' in command:
+                    led2.off() if command['led2'] == 1 else led2.on()
+                send_response('HTTP/1.1 200 OK', 'Updated Leds')
         cl.close()
 
 
+def print_dict(dict):
+    print('{')
+    for key in dict:
+        print('\t{}: {}'.format(key, dict[key]))
+    print('}')
+
+
+load_resources()
 connect()
 start_server()
 
--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
-function sendRequest(){
+function sendRequest(data){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         document.getElementById("response").innerHTML = this.responseText;
     }
     request.open('POST', 'state.json', true)
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    request.send(JSON.stringify({'led1':0, 'led2':1}))
+    request.send(JSON.stringify(data))
 }
+
+function on1(){sendRequest({'led1':1})}
+function on2(){sendRequest({'led2':1})}
+function off1(){sendRequest({'led1':0})}
+function off2(){sendRequest({'led2':0})}
