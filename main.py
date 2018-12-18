51d5d8747cc6a8d827bb1dbfb40ab79ba4fb1c3b
Grant Duffy <grantd@buchanan-a.com>
2018-12-17 20:37:48 -0800

    Got it working

--- main.py
+++ main.py
@@ -1,133 +1,75 @@
 import esp
 esp.osdebug(None)
-import time
-import socket
-import machine
 import uio
-import ure
 import json
+import machine
+import network
+import socket
+import time
 import neopixel
 
-PIXEL_COUNT = 50
+
+def send_file(cl, filename):
+    with open(filename, 'rb') as f:
+        body = b'HTTP/1.1 200 OK\r\n\r\n' + f.read()
+        n = cl.write(body)
+        print('Sent {} of {} bytes'.format(n, len(body)))
 
 led1 = machine.Pin(2, machine.Pin.OUT)
 led2 = machine.Pin(16, machine.Pin.OUT)
-color = [0, 0, 0]
-brightness = 0.1
-px = neopixel.NeoPixel(machine.Pin(15), PIXEL_COUNT)
+px = neopixel.NeoPixel(machine.Pin(15), 50)
 
-
-def send_response(status, body=''):
-    cl.send(bytes(status + '\r\n\r\n' + body, 'utf-8'))
-
-
-def parse_request(request):
-    print("GOT REQUEST:\n{}".format(request))
-    output = {}
-    with uio.BytesIO(request) as f:
-        output['Type'], output['Path'], output['Version'] = [x.decode('utf-8').strip() for x in f.readline().split()]
-        while True:
-            x = f.readline()
-            if x == b'':
-                break
-            result = ure.match(b'(.+): (.+)', x)
-            if result:
-                key = result.group(0).strip()
-                val = result.group(1).strip()
-                output[key] = val
-            if x == b'\r\n':
-                output['Body'] = f.read().decode('utf-8')
-    return output
-
-
-def load_resources():
-    global essid, password, html, javascript
-    with open('network.txt', 'r') as f:
-        essid, password = [x.strip() for x in f.readlines()]
-    with open('home.html', 'r') as f:
-        html = f.read()
-    with open('scripts.js', 'r') as f:
-        javascript = f.read()
-
-
-def connect():
-    import network
-    sta_if = network.WLAN(network.STA_IF)
-    sta_if.active(True)
+# Connect to WiFi
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+with open('network.txt', 'r') as f:
+    essid, password = [x.strip() for x in f.readlines()]
     sta_if.connect(essid, password)
-    print('Connecting', end='')
-    while not sta_if.isconnected():
-        print('.', end='')
-        led1.off()
-        led2.on()
-        time.sleep(0.1)
-        led1.on()
-        led2.off()
-        time.sleep(0.1)
-    print('Connected')
-    print(sta_if.ifconfig()[0])
+print('Connecting', end='')
+while not sta_if.isconnected():
+    print('.', end='')
     led1.off()
-    led2.off()
-    time.sleep(0.5)
-    led1.on()
     led2.on()
+    time.sleep(0.1)
+    led1.on()
+    led2.off()
+    time.sleep(0.1)
+print('Connected')
+print(sta_if.ifconfig()[0])
+led1.off()
+led2.off()
+time.sleep(0.5)
+led1.on()
+led2.on()
 
-
-def update_color():
-    print('R: {}, G: {}, B: {}'.format(*color))
-
-
-def start_server():
-    global cl, brightness, color, px
-    run = True
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', 80))
-    s.listen(5)
-    while run:
-        cl, addr = s.accept()
-        request = cl.recv(1024)
-        # print('#### GOT REQUEST ####\n{}\n'.format(request.decode('utf-8')))
-        request_data = parse_request(request)
-        if request_data['Type'] == 'GET':
-            if request_data['Path'] == '/':
-                send_response('HTTP/1.1 200 OK', html)
-            if request_data['Path'] == '/scripts.js':
-                send_response('HTTP/1.1 200 OK', javascript)
-        if request_data['Type'] == 'POST':
-            if request_data['Path'] == '/state.json':
-                command = json.loads(request_data['Body'])
-                if 'led1' in command:
-                    led1.off() if command['led1'] == 1 else led1.on()
-                if 'led2' in command:
-                    led2.off() if command['led2'] == 1 else led2.on()
-                if 'r' in command:
-                    color[0] = command['r']
-                    update_color()
-                if 'g' in command:
-                    color[1] = command['g']
-                    update_color()
-                if 'b' in command:
-                    color[2] = command['b']
-                    update_color()
-                if 'brightness' in command:
-                    brightness = command['brightness']
-                if 'r' in command or 'g' in command or 'b' in command:
-                    for i, p in enumerate(px):
-                        px[i] = color
-                    px.write()
-                send_response('HTTP/1.1 200 OK', 'Updated Leds')
+# Start Server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 80))
+s.listen(5)
+while True:
+    cl, addr = s.accept()
+    request = uio.BytesIO(cl.recv(1024))
+    first_line = request.readline()
+    if first_line == b'':
         cl.close()
-
-
-def print_dict(dict):
-    print('{')
-    for key in dict:
-        print('\t{}: {}'.format(key, dict[key]))
-    print('}')
-
-
-load_resources()
-connect()
-start_server()
-
-
+        continue
+    requst_type, path, version = first_line.decode('utf-8').split()
+    print('{} requesting {}'.format(requst_type, path))
+    x = b' '
+    while x != b'' and x != b'\r\n':
+        x = request.readline()
+        print(x)
+    if requst_type == 'GET':
+        if path == '/':
+            send_file(cl, 'home.html')
+        if path == '/scripts.js':
+            send_file(cl, 'scripts.js')
+    if requst_type == 'POST':
+        if path == '/state.json':
+            values = json.load(request)
+            print(values)
+            for i in range(50):
+                px[i] = (int(values['g']), int(values['r']), int(values['b']))
+            px.write()
+            cl.write(b'HTTP/1.1 200 OK\r\n\r\n')
+    cl.close()
--- scripts.js
+++ scripts.js
@@ -3,8 +3,7 @@ function sendRequest(data){
     request.onreadystatechange = function() {
         document.getElementById("response").innerHTML = this.responseText;
     }
-    request.open('POST', 'state.json', true)
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.open('POST', '/state.json', false)
     request.send(JSON.stringify(data))
 }
 
@@ -14,24 +13,19 @@ function updateColor(){
     b = document.getElementById("blue").value;
     sendRequest({
         'r': r,
-        'g': b,
+        'g': g,
         'b': b
     })
-    color = "rgb(" + r.toString(16) + ", " + g.toString(15) + ", " + b.toString(15) + ")"
+    color = "rgb(" + r + ", " + g + ", " + b + ")"
     return color
 }
 
-function on1(){sendRequest({'led1':1});}
-function on2(){sendRequest({'led2':1});}
-function off1(){sendRequest({'led1':0});}
-function off2(){sendRequest({'led2':0});}
-
 function onRed(){
-    document.getElementById("sliders").style.backgroundColor = updateColor();
+    updateColor();
 }
 function onGreen() {
-    document.getElementById("sliders").style.backgroundColor = updateColor();
+    updateColor();
 }
 function onBlue() {
-    document.getElementById("sliders").style.backgroundColor = updateColor();
+    updateColor();
 }
